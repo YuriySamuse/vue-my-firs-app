100955cfc8e0aa8d5f4858b4bfd43c0767c6b732
YuriySamusew <6177385@gmail.com>
2023-08-17 19:47:18 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <Button @click="increment">Cleck me</Button>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+        Button,
+  },
+  data() {
+    return {
+      amountOfClicks: 0
+    }
+  },
+  computed: {
+    title() {
+      return `Amount of clicks: ${this.amountOfClicks}`
+    }
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 1
+    }
+  },
 }
 </script>
 
